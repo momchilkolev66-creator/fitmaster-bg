a09a1b5001289ebe84fe6c80e1e031208b7eb751
Momchil Kolev <momchilkolev66@gmail.com>
2026-05-20 12:33:31 +0300

    Added bench press to exerciseMedia

--- src/data/exerciseMedia.ts
+++ src/data/exerciseMedia.ts
@@ -1,6 +1,10 @@
 export const exerciseMedia = {
-  'Ходене на пътека под наклон': {
+  'Ходене на пътека': {
     image: require('../../assets/images/exercises/walking-treadmill-incline.png'),
+  },
+
+  'Лежанка с щанга': {
+    image: require('../../assets/images/exercises/bench-press.png'),
   },
 
   default: {
